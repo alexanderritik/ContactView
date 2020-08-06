ebdee807592196d0d7399fae8578b453e46ec6d5
Ritik <rakeshkusrivastav@gmail.com>
2020-08-06 16:32:33 +0530

    Initial Commit

---
+++ ContactViewWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactViewWithSwiftUI
+//
+//  Created by Ritik Srivastava on 06/08/20.
+//  Copyright © 2020 Ritik Srivastava. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
